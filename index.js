5a368cac2e0eb66e39753ac70d477f5f8c7c5a6e
Sarabb1 <sarabjot.singh@tmotions.com>
2024-09-03 11:12:21 +0530

    close button

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var x = document.getElementById("hamburger");
+const close = document.querySelector('#menu');
 x.style.display = 'none';
 
 function myFunction() {
     if (x.style.display === "none") {
       x.style.display = "block";
+      close.setAttribute('src', 'images/Close.png');
     } else {
       x.style.display = "none";
+      close.setAttribute('src', 'images/menu-button.png');
     }
   }
