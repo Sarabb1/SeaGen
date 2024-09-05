85423811c884997e6d59f845f1cc839ee068d8ff
Sarabb1 <sarabjot.singh@tmotions.com>
2024-09-05 18:48:16 +0530

    banner

--- index.js
+++ index.js
@@ -11,3 +11,24 @@ function myFunction() {
       close.setAttribute('src', 'images/menu-button.png');
     }
   }
+
+  const heroImage = document.querySelector('.mainImage');
+ 
+  function setBackground() {
+      const vpWidth = window.innerWidth;
+   
+      if (vpWidth >= 1440) {
+          heroImage.setAttribute('src', 'images/desktop-banner.png');
+      } else if (vpWidth >= 1024) {
+          heroImage.setAttribute('src', 'images/laptop-banner.png');
+      } else if (vpWidth >= 768) {
+          heroImage.setAttribute('src', 'images/tab-banner.png');
+      } else {
+          heroImage.setAttribute('src', 'images/mobile-banner.png');
+      }
+  }
+   
+  setBackground();
+   
+  window.addEventListener('resize', setBackground);
+ 
